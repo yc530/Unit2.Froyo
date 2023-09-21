5b8b08bdaefb7182fdd942f1bb7c81e48affba1a
yc530 <yoonachoi530@gmail.com>
2023-09-21 16:15:52 -0400

    created an array and object list, then console log

---
+++ froyo.js
@@ -0,0 +1,15 @@
+const flavorPrompt =
+prompt("please enter your desired flavors seperated with commas in between!");
+
+const flavors = ["vanilla", "coffee", "strawberry"];
+
+const orderedFroyo = {
+    vanilla:3,
+    coffee: 2,
+    strawberry:1
+};
+console.log(orderedFroyo);
+
+
+// const userFlavor=
+// JSON.parse(flavorPrompt);
