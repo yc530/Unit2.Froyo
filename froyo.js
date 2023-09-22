30077f31314594e32481abdf77e47961db43a36c
yc530 <yoonachoi530@gmail.com>
2023-09-21 21:07:15 -0400

    commented mistakes and used split method

--- froyo.js
+++ froyo.js
@@ -5,15 +5,23 @@ const flavorInput = flavorPrompt.split(',');
 console.log("user inputted:", flavorInput);
 
 
+
+
+
+
+
+// mistakes! 
+// originally thought i had to make an array or an object that incuded the order that was said in the prompt. 
+// i actually had to make a prompt, and then use the split method to turn user input into arrays (a list with where the first integer starts at 0)
 // const flavors = ["vanilla", "coffee", "strawberry"];
 
 
-const orderedFroyo = {
-    vanilla:3,
-    coffee: 2,
-    strawberry:1
-};
-console.log(orderedFroyo);
+// const orderedFroyo = {
+//     vanilla:3,
+//     coffee: 2,
+//     strawberry:1
+// };
+// console.log(orderedFroyo);
 
 
 
